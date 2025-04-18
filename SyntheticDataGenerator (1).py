44b767286e75ccb1af27990438c1a7f19163745b
Jtn2121 <149103703+Jtn2121@users.noreply.github.com>
2025-04-18 12:31:10 -0500

    Update SyntheticDataGenerator.py
    added random start and end dates

--- SyntheticDataGenerator (1).py
+++ SyntheticDataGenerator (1).py
@@ -1,10 +1,14 @@
 import psycopg2
 import random
+from faker import Faker
+from datetime import datetime, timedelta
+
+fake = Faker()
 
 #database connection information
 DB_NAME = "postgres"
 DB_USER = "postgres"
-DB_PASSWORD = "fireball23" 
+DB_PASSWORD = "fireball123" 
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
@@ -17,10 +21,25 @@ task_names = [
      ["User Testing", 4],
      ["Debugging",5],
      ["Coding", 6],
-     ["Software maintenance",7]
+     ["Software maintence",7]
 ]
 
 bruh = random.choice(task_names)
+print(bruh[0])
+print(bruh[1])
+
+#function to create synthetic dates
+start_date = fake.date_between(start_date = '-3M', end_date = 'today')
+
+date_gap = random.randint(1, 9)
+
+end_date = start_date + timedelta(days = date_gap)
+
+format_start = start_date.strftime('%m/%d/%Y')
+format_end = end_date.strftime('%m/%d/%Y')
+
+print(format_start)
+print(format_end)
 
 #function to create synthetic data
 def generate_synthetic_tasks (n = 100):
@@ -29,12 +48,12 @@ def generate_synthetic_tasks (n = 100):
         task = random.choice(task_names)
         task_name = task[0]
         task_id = task[1]
-        estimated_time = random.uniform(1,50)
+        estimated_time = random.randint(1,50)
         assignee_id = random.randint(1,5) #five different employees
         var1 = random.randint(1,30)
         var2 = random.randint(1,5)
         var3 = random.randint(-10,10)
-        time_total = random.uniform(estimated_time - 10, estimated_time + 10) #sets time total +- 10 of estimated time 
+        time_total = random.randint(estimated_time - 10, estimated_time + 10) #sets time total +- 10 of estimated time 
         time_total = max (1, time_total) #makes sure its at least 1
         tasks.append((task_id, task_name, estimated_time, time_total, assignee_id, var1, var2, var3))
     return tasks
@@ -57,12 +76,13 @@ try:
                 TaskId SERIAL PRIMARY KEY,
                 task_id INTEGER NOT NULL,
                 taskname TEXT NOT NULL,
-                estimated_time DOUBLE PRECISION NOT NULL,
-                total_time DOUBLE PRECISION NOT NULL,
+                estimated_time INTEGER NOT NULL,
+                total_time INTEGER NOT NULL,
                 assignee_id INTEGER NOT NULL,
                 var1 INTEGER NOT NULL,
                 var2 INTEGER NOT NULL,
-                var3 INTEGER NOT NULL
+                var3 INTEGER NOT NULL,
+                task_dates DATE NOT NULL
                        
             )
         """)
@@ -74,7 +94,7 @@ try:
         #generate synthetic tasks
         synthetic_tasks = generate_synthetic_tasks(n = 2500)
         cursor.executemany(
-             "INSERT INTO Tasks2 (task_id, taskname, estimated_time, total_time, assignee_id, var1, var2, var3) VALUES (%s,%s, %s, %s, %s, %s, %s, %s)", synthetic_tasks
+             "INSERT INTO Tasks2 (task_id, taskname, estimated_time, total_time, assignee_id, var1, var2, var3, task_dates) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s)", synthetic_tasks
         )
         conn.commit()
         print(f" Inserted {len(synthetic_tasks)} synthetic tasks")
@@ -89,5 +109,3 @@ finally:
         print("Connection closed")
 
             
-
-
